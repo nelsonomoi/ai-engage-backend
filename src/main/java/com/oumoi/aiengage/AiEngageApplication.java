27968b594547d26b97dbfc87abe04872cd6bf499
Nelson Omoi <nelson.omoi@smart.local>
2024-01-03 16:10:01 +0300

    init commit

---
+++ src/main/java/com/oumoi/aiengage/AiEngageApplication.java
@@ -0,0 +1,13 @@
+package com.oumoi.aiengage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEngageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEngageApplication.class, args);
+	}
+
+}
